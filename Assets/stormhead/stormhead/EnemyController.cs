e38813be64f4af1b5010c86703c101bbe67add35
Trieu Lai <trieulai1202@gmail.com>
2023-12-03 11:23:24 +0700

    Add new background (#1)
    * Trieu submission
    
    add more particles, change render pipeline to URP
    
    * Add new background

--- Assets/stormhead/stormhead/EnemyController.cs
+++ Assets/stormhead/stormhead/EnemyController.cs
@@ -7,20 +7,30 @@ public class EnemyController : MonoBehaviour
     public float moveSpeed = 5f;
 
     private Transform player;
+    private GameObject sprite;
+    private GameObject sprite2;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        sprite = GameObject.Find("Magnetic_sprite1");
+        // sprite2 = GameObject.Find("Magnetic_sprite2");
+        sprite.SetActive(false);
+        // sprite2.SetActive(false);
     }
 
     void Update()
     {
         // Check if player is within range
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-        if (distanceToPlayer < 10f) // Adjust the value as needed
+        if (distanceToPlayer < 5f) // Adjust the value as needed
         {
+            sprite.SetActive(true);
             // Move towards the player
             transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+        else {
+                sprite.SetActive(false);
         }
     }
 }
