eb03f82ce2547552cc9b017f1f8f8e1a4014e25f
tintin117 <baolongnh117@gmail.com>
2023-12-05 13:10:22 +0700

    merged test

--- Assets/stormhead/stormhead/EnemyController.cs
+++ Assets/stormhead/stormhead/EnemyController.cs
@@ -4,53 +4,33 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+        public float moveSpeed = 5f;
 
-    private Transform player;
-    private GameObject sprite;
-    private GameObject sprite2;
+        private Transform player;
+        private GameObject sprite;
+        private GameObject sprite2;
 
-    void Start()
-    {
-<<<<<<< HEAD
-        //player = GameObject.FindGameObjectWithTag("Player").transform;
-        //sprite = GameObject.Find("Magnetic_sprite1");
-        // sprite2 = GameObject.Find("Magnetic_sprite2");
-       // sprite.SetActive(false);
-=======
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        sprite = GameObject.Find("Magnetic_sprite1");
-        // sprite2 = GameObject.Find("Magnetic_sprite2");
-        sprite.SetActive(false);
->>>>>>> main
-        // sprite2.SetActive(false);
-    }
-
-    void Update()
-    {
-        // Check if player is within range
-<<<<<<< HEAD
-        //float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-        //if (distanceToPlayer < 5f) // Adjust the value as needed
-        //{
-        //    sprite.SetActive(true);
-        //    // Move towards the player
-        //    transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
-        //}
-        //else {
-        //        sprite.SetActive(false);
-        //}
-=======
-        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-        if (distanceToPlayer < 5f) // Adjust the value as needed
+        void Start()
         {
-            sprite.SetActive(true);
-            // Move towards the player
-            transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+                //player = GameObject.FindGameObjectWithTag("Player").transform;
+                //sprite = GameObject.Find("Magnetic_sprite1");
+                // sprite2 = GameObject.Find("Magnetic_sprite2");
+                // sprite.SetActive(false);
+                // sprite2.SetActive(false);
         }
-        else {
-                sprite.SetActive(false);
+
+        void Update()
+        {
+                // Check if player is within range
+                //float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+                //if (distanceToPlayer < 5f) // Adjust the value as needed
+                //{
+                //    sprite.SetActive(true);
+                //    // Move towards the player
+                //    transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+                //}
+                //else {
+                //        sprite.SetActive(false);
+                //}
         }
->>>>>>> main
-    }
 }
